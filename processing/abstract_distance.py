fixed class/instance vars, added month to calculation of relative publication time

processing/abstract_distance.py
@@ -19,19 +19,19 @@ def save_abstracts_and_titles(author, retmax=100):
     dois = pubmed_interface.search_pubmed_author(author, retmax=retmax)
     titles = []
     abstracts = []
-    years = []
+    years_months = []
     for doi in dois:
         article = pubmed_interface.PubMedObject(doi)
         article.download()
         article.fill_data()
         titles.append(article.title)
         abstracts.append(article.abstract)
-        years.append(article.pub_year)
+        years_months.append((article.pub_year, article.pub_month))
         
     with open('%s.txt'%author.replace(' ', '') , 'w') as f:
-        json.dump({'titles': titles, 'abstracts': abstracts, 'retmax': retmax, 'years': years}, f)
+        json.dump({'titles': titles, 'abstracts': abstracts, 'retmax': retmax, 'years_months': years_months}, f)
     
-    return titles, abstracts, years
+    return titles, abstracts, years_months
 
 
 def tokenize(text):
@@ -40,7 +40,7 @@ def tokenize(text):
     return stems
 
 
-def get_dataset(titles, abstracts, years):
+def get_dataset(titles, abstracts, years_months):
     #separate words with dashes - and then remove punctuation. 
     abstracts = [ab.lower().translate(str.maketrans("-", " ")) for ab in abstracts]
     abstracts = [''.join(c for c in ab if c not in set(string.punctuation)) for ab in abstracts]
@@ -67,8 +67,13 @@ def get_dataset(titles, abstracts, years):
     ordered_distance_matrix = ordered_distance_matrix[:, ordering]
     ordered_distance_matrix = ordered_distance_matrix[ordering, :]
     
-    years = np.array([int(y) if y != '' else np.NaN for y in years])
-    norm_years = (years - np.nanmin(years))/(np.nanmax(years) - np.nanmin(years))
+    years, months = zip(*years_months)
+    years = np.array([int(y) if y != '' else np.NaN for y in years]).astype('float')
+    months = np.array([int(m) if m != '' else np.NaN for m in months]).astype('float')
+    months[np.isnan(years)] = np.NaN
+    
+    pub_time = years + (months - 1)/12
+    norm_years = (pub_time - np.nanmin(pub_time))/(np.nanmax(pub_time) - np.nanmin(pub_time))
     years[np.isnan(years)] = -1
     norm_years[np.isnan(norm_years)] = -1
     years = years.tolist()